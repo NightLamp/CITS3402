101864ead9f997d0384adb9258cc6a1745f9ce10
nightlamp <benjaminlongbottom98@gmail.com>
2019-10-20 16:42:20 +0800

    added new funcs into header

--- include/project.h
+++ include/project.h
@@ -16,17 +16,34 @@ typedef struct matrix {
     int **array;
 } MATRIX;
 
+
+typedef struct _sub_matrix {
+	int fullSize;
+	int localSize;
+	int nodeOffset;
+	int **array;
+} SUB_MATRIX;
+
 //reading the file
-int read_file(MATRIX* m, char *filepath);
+int read_file(MATRIX *m, char *filepath);
+int read_file_distributed(SUB_MATRIX *m, char *filepath);
+int get_local_node_count(int p, int pc, int nc);
+int get_node_offset(int p, int pc, int nc);
 
 //memory functions
 void *handle_malloc(size_t size);
 void *handle_realloc(void *ptr, size_t size);
+void copy_array(int * to, int * from, int length);
 
 //debug
 void debug();
 void print_matrix(MATRIX *m);
+void print_matrix_distributed(SUB_MATRIX * sm);
+//void print_sub_matrix(SUB_MATRIX *m);
+void print_array(int *arr, int length);
 
 //floyd-warshall
 int min(int a, int b);
 int floyd_warshall(MATRIX * m);
+int floyd_warshall_distributed(SUB_MATRIX * sm);
+
